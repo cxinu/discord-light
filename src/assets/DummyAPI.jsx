b429e6f253f93ffbb61fb2ebc28b085aa465c035
cxinu <cxinu3099@gmail.com>
2023-06-26 05:20:01 +0530

    rm normie chat

--- src/assets/DummyAPI.jsx
+++ src/assets/DummyAPI.jsx
@@ -24,108 +24,66 @@ export const channels = [
 
 export const chatMessages = [
   {
-    name: "User123",
-    timestamp: "Today at 9:00 AM",
-    text: "Hey everyone, how's it going?",
+    name: "Just Ali",
+    timestamp: "Today at 4:35 PM",
+    text: "Why do we have Animated profile pictures?",
   },
   {
-    name: "LunaStar",
-    timestamp: "Today at 9:02 AM",
-    text: "I'm good! Just finished my morning workout. 💪",
+    name: "mouse",
+    timestamp: "Today at 4:35 PM",
+    text: "Idk must be the developer.",
   },
   {
-    name: "TechGeek",
-    timestamp: "Today at 9:05 AM",
-    text: "Morning! I'm working on a cool new project. Any ideas for a catchy name?",
+    name: "Bonjoer_",
+    timestamp: "Today at 4:35 PM",
+    text: "nitroLord u don't need nitro here",
+  },
+  { name: "IgnoredDeus", timestamp: "Today at 4:35 PM", text: "hello" },
+  {
+    name: "Disappointment",
+    timestamp: "Today at 4:35 PM",
+    text: "Where ur keyboard at",
+  },
+  { name: "JERRY", timestamp: "Today at 4:35 PM", text: "Nice app btw" },
+  {
+    name: "Copium+Hopium=CSM",
+    timestamp: "Today at 4:35 PM",
+    text: "What did we pay nitro for?",
+  },
+  { name: "Lost In The Sauce", timestamp: "Today at 4:35 PM", text: "Scammed" },
+  {
+    name: "volox",
+    timestamp: "Today at 4:35 PM",
+    text: "Yes I want my two dollars back",
   },
   {
-    name: "MusicLover",
-    timestamp: "Today at 9:07 AM",
-    text: "How about 'HarmonyHub' or 'SoundWaveTech'?",
+    name: "My_2_dollar",
+    timestamp: "Today at 4:35 PM",
+    text: "yeah I'm here, Now what.",
   },
   {
-    name: "CoffeeAddict",
-    timestamp: "Today at 9:10 AM",
-    text: "Ugh, I need more coffee to wake up properly. ☕",
+    name: "Neeko Neeko Niii",
+    timestamp: "Today at 4:36 PM",
+    text: "Holy, What have I witnessed, Who summoned you Where've you been ?",
   },
   {
-    name: "Bookworm",
-    timestamp: "Today at 9:12 AM",
-    text: "Any book recommendations? I'm in the mood for a thrilling mystery!",
+    name: "noGanyu",
+    timestamp: "Today at 4:35 PM",
+    text: "Oh, look what you've done now.",
   },
   {
-    name: "AdventureSeeker",
-    timestamp: "Today at 9:15 AM",
-    text: "Have you guys ever been skydiving? I'm thinking of trying it out!",
+    name: "Gon Solos",
+    timestamp: "Today at 4:46 PM",
+    text: "So, people still believe in santa claus?",
   },
+  { name: "ZERO", timestamp: "Today at 4:48 PM", text: "bruh its october" },
+  { name: "CraZe", timestamp: "Today at 4:51 PM", text: "Merry crisis" },
   {
-    name: "Foodie",
-    timestamp: "Today at 9:18 AM",
-    text: "I just tried this new restaurant in town, and the food was amazing! Highly recommend it.",
-  },
-  {
-    name: "GamerGuy",
-    timestamp: "Today at 9:21 AM",
-    text: "Who's up for a gaming session tonight? I'm thinking of playing some Overwatch.",
-  },
-  {
-    name: "PetLover",
-    timestamp: "Today at 9:25 AM",
-    text: "Look at this adorable picture of my new puppy! 🐶",
-  },
-  {
-    name: "MovieBuff",
-    timestamp: "Today at 9:28 AM",
-    text: "I just watched an incredible movie last night. The plot twist blew my mind!",
-  },
-  {
-    name: "FitnessGuru",
-    timestamp: "Today at 9:32 AM",
-    text: "Remember to stay active and take care of your health, everyone. You got this!",
-  },
-  {
-    name: "TechGeek",
-    timestamp: "Today at 9:35 AM",
-    text: "Thanks for the name suggestions, MusicLover. I'm leaning towards 'SoundWaveTech' now.",
-  },
-  {
-    name: "User123",
-    timestamp: "Today at 9:40 AM",
-    text: "I'm loving all the interesting conversations here. This community is awesome!",
-  },
-  {
-    name: "LunaStar",
-    timestamp: "Today at 9:45 AM",
-    text: "Agreed, User123! It's great to connect with like-minded people.",
-  },
-  {
-    name: "CoffeeAddict",
-    timestamp: "Today at 9:50 AM",
-    text: "I finally got my second cup of coffee. Now I can conquer the world! 😄",
-  },
-  {
-    name: "Bookworm",
-    timestamp: "Today at 9:55 AM",
-    text: "For a thrilling mystery, I recommend 'Gone Girl' by Gillian Flynn. It's a page-turner!",
-  },
-  {
-    name: "AdventureSeeker",
-    timestamp: "Today at 10:00 AM",
-    text: "Skydiving is an incredible experience! You'll love the adrenaline rush, I promise.",
-  },
-  {
-    name: "Foodie",
-    timestamp: "Today at 10:05 AM",
-    text: "I'll definitely check out that restaurant, thanks for the recommendation!",
-  },
-  {
-    name: "GamerGuy",
-    timestamp: "Today at 10:10 AM",
-    text: "Count me in for the gaming session tonight! Overwatch sounds fun.",
+    name: "shed 鬼",
+    timestamp: "Today at 4:58 PM",
+    text: "Id rather not, i dont really care what you have to say about it because neither of us will walk out with a changed mind",
   },
 ];
-
-// Add more chat messages below...
 
 export const methods = [
   "wave",
